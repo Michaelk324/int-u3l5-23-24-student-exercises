d34f06dd1a6bbcbda6fe445d1b7a47360cc5c71e
Michaelk324 <147104243+Michaelk324@users.noreply.github.com>
2024-01-09 22:04:19 +0000

    DOM Manipulation

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ let img = document.querySelector("img");
 // 1. Press the button on the webpage to change it up! Take notice of what happens.
 // - What do you think .style does?
 
+
 // 2. Try the following:
 // - Make it so that the colors are different than what's used.
 // - Try using a hex color code.
 button.addEventListener("click", function() {
-    body.style.backgroundColor = "black";
-    header.style.color = "white";
+    body.style.backgroundColor = "#73bbbd";
+    header.style.color = "#e6f5b5";
     img.style.border = "5px solid white";
 });
